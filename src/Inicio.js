12d42158f0c3a506939749cd380cf788c4cb11e7
victoriajdias <134943386+victoriajdias@users.noreply.github.com>
2023-09-04 09:27:58 -0300

    Add files via upload

---
+++ src/Inicio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+
+const User = () => {
+
+
+  
+    return (
+        <div className="HomeContainer">
+        <div className="HomeContent">
+      <div>
+        
+        Tela de Usuário 
+      </div>
+      </div>
+    </div>
+    )
+  }
+  
+  export default User
